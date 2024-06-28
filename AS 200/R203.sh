732573a039bd5b767a856fdcf5ccba3334df9c9f
Michele Tosi <micheletosi98@gmail.com>
2024-06-28 13:55:11 +0200

    Update R203.sh

--- AS 200/R203.sh
+++ AS 200/R203.sh
@@ -17,10 +17,13 @@ iptables -A FORWARD -i $LAN -p tcp --dport 80 -j ACCEPT
 iptables -A FORWARD -i $LAN -p tcp --dport 443 -j ACCEPT
 iptables -A FORWARD -i $LAN -p tcp --dport 22 -j ACCEPT
 iptables -A FORWARD -i $LAN -p udp --dport 53 -j ACCEPT
+iptables -A FORWARD -i $LAN -p udp --dport 1194 -j ACCEPT
+
 iptables -A INPUT -m state --state ESTABLISHED -j ACCEPT
 iptables -A INPUT -i $LAN -p tcp --dport 22 -j ACCEPT
 iptables -A INPUT -i $WAN -p tcp --dport 22 -j ACCEPT
 iptables -A INPUT -p icmp -j ACCEPT
+iptables -A INPUT -i $LAN -p udp --dport 1194 -j ACCEPT
 iptables -A FORWARD -p icmp -j ACCEPT
 
 iptables -t nat -A POSTROUTING -o eth1 -j MASQUERADE
